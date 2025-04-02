3026738857f198d670b0ee931d6d6fd2ba92f835
Vitaliy <vitaliy.yanitskyy@gmail.com>
2025-04-02 12:42:51 +0300

    Add AboutController

---
+++ TestTask.Server/Models/Response/AboutContentResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTask.Server.Models.Response;
+
+public class AboutContentResponse
+{
+    public string Content { get; set; } = string.Empty;
+    public DateTime? LastUpdatedDate { get; set; }
+    public string LastUpdatedBy { get; set; } = string.Empty;
+}
