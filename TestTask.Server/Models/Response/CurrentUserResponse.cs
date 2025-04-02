e1652878443003bd9bac33dbf4315eb57adcf38e
Vitaliy <vitaliy.yanitskyy@gmail.com>
2025-04-02 11:49:21 +0300

    Add UserController

---
+++ TestTask.Server/Models/Response/CurrentUserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTask.Server.Models.Response;
+
+public class CurrentUserResponse
+{
+    public bool IsAuthenticated { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public bool IsAdmin { get; set; }
+}
